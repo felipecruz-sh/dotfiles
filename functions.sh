3fe79288f1a55bb925b6ec0723cdbe2201099b5a
felipecruz-it <felipecruz.d@proton.me>
2024-03-17 18:52:08 -0300

    20240317

--- functions.sh
+++ functions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-nvidia_detect()
+NvidiaDetect()
 {
     if [ "$(lspci -k | grep -A 2 -E "(VGA|3D)" | grep -c nvidia)" -gt 0 ]
     then
@@ -19,4 +19,35 @@ PacmanConf()
     sed -i "/^#Color\|^#ParallelDownloads\|^#VerbosePkgLists\|^#\[multilib\]/s/^#//" $PacmanDir
     sed -i "$IncludeLine s/#//" $PacmanDir
     sed -i '/^Color/a ILoveCandy' $PacmanDir
+}
+
+
+GrubConfig()
+{   
+mv /etc/default/grub /etc/default/grub.bak
+
+cat > /etc/default/grub << EOF
+GRUB_DEFAULT=0
+GRUB_TIMEOUT_STYLE=hidden
+GRUB_TIMOUT=0
+GRUB_DISTRIBUTOR="Arch"
+GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"
+GRUB_CMDLINE_LINUX=""
+#GRUB_DISABLE_OS_PROBER=false
+#GRUB_TERMINAL=console
+#GRUB_GFXMODE=auto
+#GRUB_DISABLE_LINUX_UUID=true
+#GRUB_DISABLE_RECOVERY="true"
+#GRUB_INIT_TUNE="480 440 1"
+EOF
+
+grub-mkconfig -o /boot/grub/grbu.cfg
+}
+
+BootSplashScreen()
+{
+    sed -i '/HOOKS=/ s/udev /udev plymouth ' /etc/mkinitcpio.conf
+    sed -i '/^Theme/a ShowDelay=0' /etc/plymouth/plymouthd.conf
+    mkinitcpio -p linux-lts
+    plymouth-set-default-theme -R bgrt
 }
