f59ff3453bfe4d2502bf2b4f7726d42575ef588b
felipecruz-it <felipecruz.d@proton.me>
2024-03-18 04:52:52 -0300

    20240318

--- functions.sh
+++ functions.sh
@@ -47,9 +47,13 @@ grub-mkconfig -o /boot/grub/grub.cfg
 BootSplashScreen()
 {
     sed -i '/^HOOKS/ s/udev /udev plymouth /' /etc/mkinitcpio.conf
+    
     mkinitcpio -p linux-lts
-    sed -i '/^Theme/a ShowDelay=0' /etc/plymouth/plymouthd.conf
+    
     plymouth-set-default-theme -R bgrt
+
+    sed -i '/^Theme/a ShowDelay\=0' /etc/plymouth/plymouthd.conf
+    
     sed -i '/message/d' /etc/grub.d/10_linux
 }
 
