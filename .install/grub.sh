405620b39339d147810e13bb2cfdd82854f5361f
felipecruz-it <felipecruz.d@proton.me>
2024-03-24 13:14:10 -0300

    update grub.sh

--- .install/grub.sh
+++ .install/grub.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
-GrubConfig() {   
-mv /etc/default/grub /etc/default/grub.bak
+Lines='GRUB_DEFAULT=0
+	GRUB_TIMEOUT_STYLE=hidden
+	GRUB_TIMEOUT=0
+	GRUB_DISTRIBUTOR="Arch"
+	GRUB_CMDLINE_LINUX_DEFAULT="quiet vga=current splash"
+	GRUB_CMDLINE_LINUX=""
+	#GRUB_DISABLE_OS_PROBER=false
+	#GRUB_TERMINAL=console
+	#GRUB_GFXMODE=auto
+	#GRUB_DISABLE_LINUX_UUID=true
+	#GRUB_DISABLE_RECOVERY="true"
+	#GRUB_INIT_TUNE="480 440 1"'
 
-cat > /etc/default/grub << EOF
-GRUB_DEFAULT=0
-GRUB_TIMEOUT_STYLE=hidden
-GRUB_TIMOUT=0
-GRUB_DISTRIBUTOR="Arch"
-GRUB_CMDLINE_LINUX_DEFAULT="quiet vga=current splash"
-GRUB_CMDLINE_LINUX=""
-#GRUB_DISABLE_OS_PROBER=false
-#GRUB_TERMINAL=console
-#GRUB_GFXMODE=auto
-#GRUB_DISABLE_LINUX_UUID=true
-#GRUB_DISABLE_RECOVERY="true"
-#GRUB_INIT_TUNE="480 440 1"
-EOF
+function GrubConfig() {
+    if grub-install --version >&- && [ -f /boot/grub/grub.cfg ]; then
+        mv /etc/default/grub /etc/default/grub.bak
 
-grub-mkconfig -o /boot/grub/grub.cfg
+        if echo "${Lines//  /}" >> grub; then
+            grub-mkconfig -o /boot/grub/grub.cfg
+        fi
+    fi
 }
